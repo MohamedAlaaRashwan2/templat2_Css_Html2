585ab8395ae9781312e505e26ed51d3542271ef4
Mohamed Rashwan <mohaalaa@gmail.com>
2025-03-02 16:42:49 +0200

    edtt3

--- main.js
+++ main.js
@@ -6,3 +6,47 @@ window.addEventListener("scroll", () => {
     const scrollTop = document.documentElement.scrollTop;
     el.style.width = `${(scrollTop / height) * 100}%`;
 });
+
+document.getElementById("buttonVideo").addEventListener("click", function () {
+    window.location.href = '#landing';
+});
+
+let links = document.querySelectorAll("ul.mmaa li a");
+
+links.forEach(function (ele) {
+    ele.onclick = function () {
+        links.forEach(function (ele) {
+
+            ele.classList.remove("active");
+        });
+
+        this.classList.add("active");
+    };
+});
+
+
+let links2 = document.querySelectorAll("ul.shuffle li ");
+
+links2.forEach(function (ele) {
+    ele.onclick = function () {
+        links2.forEach(function (ele) {
+            ele.classList.remove("active")
+        });
+        this.classList.add("active");
+    };
+});
+
+let toggleMenu = document.querySelector(".toggle-menu");
+let nav = document.querySelector("header nav .toggle-menu");
+let closeMenu = document.querySelector("#toggle-menu");
+
+
+toggleMenu.onclick = function () {
+    nav.classList.add("open");
+};
+
+closeMenu.onclick = function () {
+    nav.classList.remove("open");
+};
+
+
